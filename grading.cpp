6d12539b282daebcd626d2f927229a82af1a31a5
Patrickdevaney01 <72946449+Patrickdevaney01@users.noreply.github.com>
2020-11-13 17:34:15 +0000

    Create grading.cpp
    Grading Program

---
+++ grading.cpp
@@ -0,0 +1,12 @@
+        cout << "Congralulations you got an A "<<endl;
+    else if(score >= 80 & score < 90)
+        cout << "Congralulations you got an B "<<endl;
+    else if(score >= 70 & score < 80)
+        cout << "Congralulations you got an C "<<endl;
+    else if(score >= 60 & score < 70)
+        cout << "Congralulations you got an D "<<endl;
+    else if(score < 60)
+        cout << "Unfortunately you got an F "<<endl;
+
+    return 0;
+}
